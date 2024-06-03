c78eb985db7b09dcc4a7edf8515ffd965757094f
ext_iestrada <ext_iestrada@tottus.com.pe>
2024-06-03 16:10:11 -0500

    Validation JWT

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,18 @@
+const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
 
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    // console.log(client.handshake.headers['x-token']);
+    const [valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
+    // console.log(valido, uid);
+
+    if (!valido){
+        return client.disconnect();
+    }
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
