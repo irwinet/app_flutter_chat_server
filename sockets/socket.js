80454bb98a51dbac19de10de37c0d321f9e14c18
ext_iestrada <ext_iestrada@tottus.com.pe>
2024-06-03 16:17:41 -0500

    Add socket controller

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,21 +1,24 @@
 const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
-
+const { usuarioConectado, usuarioDesconectado } = require('../controllers/socket');
 
 // Mensajes de Sockets
-io.on('connection', client => {
+io.on('connection', (client) => {
     console.log('Cliente conectado');
 
     // console.log(client.handshake.headers['x-token']);
     const [valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
     // console.log(valido, uid);
 
-    if (!valido){
+    if (!valido) {
         return client.disconnect();
     }
 
+    usuarioConectado(uid);
+
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
+        usuarioDesconectado(uid);
     });
 
     // client.on('mensaje', ( payload ) => {
